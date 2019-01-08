cab1487f55f5788e32cb6308ebc4f2b19f5b4a7b
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2019-01-08 04:47:04 -0800

    Update guildUpdate.js

--- events/guildUpdate.js
+++ events/guildUpdate.js
@@ -6,9 +6,8 @@ module.exports.run = async (bot, oldGuild, newGuild) => {
           .setTimestamp()
           .setAuthor(`Server Updated`, newGuild.iconURL)
           .addField(`Info`, `**Name:** ${newGuild.name}\n**ID:** ${newGuild.id}`)
-
-        if(oldGuild.name !== newGuild.name) {embed.addField(`Name Change`, `**Before:** ${oldGuild.name}\n**After:** ${newGuild.name}`)}
-        if(oldGuild.iconURL !== newGuild.iconURL){embed.addField(`Icon Change`, `**Before:** ${oldGuild.iconURL ? `[Click Here](${oldGuild.iconURL})` : "None"}\n**After:** ${newGuild.iconURL ? `[Click Here](${newGuild.iconURL})` : "None"}`)}
+        if(oldGuild.name !== newGuild.name) {embed.addField(`Name Change`, `**Before:** ${oldGuild.name}\n**After:** ${newGuild.name}`)}else
+        if(oldGuild.iconURL !== newGuild.iconURL){embed.addField(`Icon Change`, `**Before:** ${oldGuild.iconURL ? `[Click Here](${oldGuild.iconURL})` : "None"}\n**After:** ${newGuild.iconURL ? `[Click Here](${newGuild.iconURL})` : "None"}`)}else{return;}
         bot.functions.server(bot, oldGuild, embed)
     }catch(e){
         bot.logger(bot, oldGuild, e.stack)
