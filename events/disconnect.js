9605ef56b6ad485a51896dae357d1a6dfeb36357
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2019-01-09 10:38:01 -0800

    Update disconnect.js

--- events/disconnect.js
+++ events/disconnect.js
@@ -7,4 +7,10 @@ module.exports.run = async (client) => {
     .setTimestamp()
     client.channels.get(client.config.log).send(e)
     console.log(`${client.user.tag} (${client.user.id}) has been disconnected.`)
+    let embed = new RichEmbed()
+    .setAuthor(client.user.tag, client.user.displayAvatarURL)
+    .setColor(client.util.colors.red)
+    .setTitle(`Disconnected`)
+    .setTimestamp()
+    client.channels.get("532628525640056852").send(embed)
 }
