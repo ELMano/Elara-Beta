ed7010be4a9698c01a5c564bcb13b5fc0b6be334
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2019-01-10 03:03:30 -0800

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -5,12 +5,12 @@ module.exports.run = (client, member) => {
     try{
     let botembed = new Discord.RichEmbed()
         .setColor(`#FF000`)
-        .setTitle(`Member Joined`)
-        .setAuthor(member.user.tag, member.user.displayAvatarURL)
+        .setAuthor(`Member Joined${client.isOwner(member.user.id) ? ' - 🛡 Bot Owner' : ''}`, member.user.displayAvatarURL)
+        .setTitle(`Userinfo`)
         .setDescription(`**Mention: **${member.user}\n**Tag: **${member.user.tag}\n**ID: **${member.id}\n**Created At: ${moment(member.user.createdAt).format('dddd, MMMM Do YYYY')}**\n**MemberCount: **${member.guild.memberCount}`)
         .setThumbnail(member.user.displayAvatarURL)
     if (member.user.username.includes("discord.gg/") || member.user.username.includes("discord.me/")) { botembed.addField(`Invite Detected`, `This user has a invite in their username`) }
-    client.functions.joins(client, member.guild, botembed)
+    client.log(client, member.guild, botembed)
     }catch(e){
         client.logger(client, member.guild, e.stack)
     }
