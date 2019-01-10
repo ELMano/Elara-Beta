216c570170ac8f5941b9ba839533e2417a39f754
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2019-01-10 01:45:46 -0800

    Update 8ball.js

--- commands/Fun Commands/Normal Fun Commands/8ball.js
+++ commands/Fun Commands/Normal Fun Commands/8ball.js
@@ -1,5 +1,7 @@
 const { Command } = require('../../../util/Commando'),
     Discord = require('discord.js');
+const client = require('nekos.life');
+const neko = new client();
 module.exports = class NCommand extends Command {
     constructor(client) {
         super(client, {
@@ -21,15 +23,18 @@ module.exports = class NCommand extends Command {
     async run(message, { ball }) {
         this.client.stats(this.client, "cmd", null, null, null)
         try{
-        let answer = [`Yes`, `No`, `Maybe`, `Nope`, `Never`, `Ask again later.`, `Absolutely`, `Not now`, `It will pass`, `Go for it`, `Wait for it`, `No doubt`];
-        let result = Math.floor(Math.random() * answer.length);
+        let n = await neko.getSFW8Ball()
+        let res = n.response;
+        let link = n.url;
+        if(res.toLowerCase() === "you're hot") res = "Ask Again";
+        if(link === "https://cdn.nekos.life/8ball/Youre_hot.png") link = "https://cdn.nekos.life/8ball/Ask_Again.png"  
         let embed = new Discord.RichEmbed()
             .setColor("RANDOM")
-            .setDescription(`**Question:** ${ball}\n**Answer:** ${answer[result]}`)
-            .setImage(answer[result]);
+            .setDescription(`**Question:** ${ball}\n**Answer:** ${res}`)
+            .setImage(link);
         message.channel.send(embed);
         } catch (e) {
-            this.client.error(this.client, message, e);
+            this.client.error(this.client, message, e)
             this.client.logger(this.client, message.guild, e.stack, message, message.channel)
         }
     }
