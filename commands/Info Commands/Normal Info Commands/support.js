17f4e0ba368f262debaa0325ae1af5d2b2affbf2
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2019-01-23 09:35:37 -0800

    Update support.js

--- commands/Info Commands/Normal Info Commands/support.js
+++ commands/Info Commands/Normal Info Commands/support.js
@@ -15,9 +15,10 @@ module.exports = class SCommand extends Command {
         this.client.stats(this.client, "cmd", null, null, null)
         try{
         let embed = new Discord.RichEmbed()
-            .setAuthor(`Elara Support`, this.client.util.avatar)
+            .setAuthor(`Elara Support`, "https://cdn.discordapp.com/icons/499409162661396481/28c6fa39e722e2c0aea60f15ca105c1d.webp?size=2048")
             .setColor(`RANDOM`)
-            .setDescription(`[Click Here](${this.client.options.invite})`)
+            .setThumbnail(`https://cdn.discordapp.com/icons/499409162661396481/28c6fa39e722e2c0aea60f15ca105c1d.webp?size=2048`)
+            .setDescription(`[Support Server](${this.client.options.invite})`)
         message.say(embed)
         } catch (e) {
             this.client.error(this.client, message, e);
